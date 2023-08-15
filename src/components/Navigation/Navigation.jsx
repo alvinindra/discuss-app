42789a32095988c7d4c44d3880a7b68d319847a2
Alvin Indra <alvin@dicoding.com>
2023-08-15 11:40:02 +0700

    style: improve styling

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,9 +4,13 @@ export default function Navigation() {
   return (
     <div className="flex flex-wrap">
       <Card className="mb-4 w-100">
-        <div className="border-b-1 border-0 border-solid mb-3">
+        <div className="border-b-1 border-0 border-solid mb-3 border-black">
           <div className="flex mb-4">
-            <img src="https://placehold.co/36x36" className="rounded-full me-3" alt="" />
+            <img
+              src="https://ui-avatars.com/api/?name=Leonanta+Pramudya"
+              className="rounded-full me-3 w-36px h-36px"
+              alt=""
+            />
             <div className="flex mx-auto">
               <div className="my-auto text-3.5 font-medium">Leonanta Pramudya</div>
             </div>
@@ -20,7 +24,7 @@ export default function Navigation() {
       </Card>
       <a
         href="#"
-        className="decoration-none bg-[#66BD7E] w-100 border-0 py-3 border-rounded-2 text-white font-semibold text-center mb-4"
+        className="decoration-none bg-brand-primary w-100 border-0 py-3 border-rounded-2 text-white font-semibold text-center mb-4"
       >
         New Thread
       </a>
