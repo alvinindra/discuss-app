adf43ab3fd783e42ad212dfc8d3f9fdd04db1fff
Alvin Indra <alvin@dicoding.com>
2023-08-15 15:02:04 +0700

    feat: protect user auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
+import { useDispatch } from 'react-redux'
+import { useEffect } from 'react'
+import { asyncGetAuthUser } from './states/auth/action'
 
 function App() {
+  const dispatch = useDispatch()
+  const isAuthenticated = localStorage.getItem('accessToken')
+
+  useEffect(() => {
+    dispatch(asyncGetAuthUser())
+  }, [dispatch])
+
   return (
     <Routes>
       <Route>
         <Route path="/*" element={<HomePage />}></Route>
         <Route path="/" element={<HomePage />}></Route>
-        <Route path="/login" element={<LoginPage />}></Route>
-        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/login" element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />} />
+        <Route
+          path="/register"
+          element={isAuthenticated ? <Navigate to="/" /> : <RegisterPage />}
+        />
       </Route>
     </Routes>
   )
