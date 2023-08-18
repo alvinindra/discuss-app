adaccde8893d56a600aee1cd4d59f51e5698e875
Alvin Indra <alvin@dicoding.com>
2023-08-18 11:26:06 +0700

    style: adjust space body

--- src/pages/CreatePage.jsx
+++ src/pages/CreatePage.jsx
@@ -5,7 +5,7 @@ import Tags from '@/components/Tags/Tags'
 function CreatePage() {
   return (
     <div className="bg-brand-secondary">
-      <div className="sm:container mx-auto pt-[78px] min-h-100vh grid grid-cols-4">
+      <div className="sm:container mx-auto pt-[48px] min-h-100vh grid grid-cols-4">
         <div className="sm:col-span-3 col-span-3 px-4">
           <Card>
             <h1>Create new discussion</h1>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -40,7 +40,7 @@ function HomePage() {
 
   return (
     <div className="bg-brand-secondary">
-      <div className="sm:container mx-auto pt-[78px] min-h-100vh grid grid-cols-4">
+      <div className="sm:container mx-auto pt-[48px] min-h-100vh grid grid-cols-4">
         <div className="sm:col-span-3 col-span-3 px-4">
           <HeaderBanner />
           <Discussions discussionsList={discussionsList} />
